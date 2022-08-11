f47eb338190f67b7ed2505a77abe9609d3889153
samijain26 <samijain26@yahoo.com>
2022-08-12 01:35:19 +0530

    for and while loop

---
+++ loops/app.js
@@ -0,0 +1,115 @@
+//for loop
+
+for (let i=10;i>0;i--){
+    console.log(i)
+}
+//break: to break the loop
+//continue  : to go back the loop and ignore further commands for that condition
+// for even and odd number
+
+
+
+
+
+
+    //Ex2:
+    console.log('Ex2')
+for(let i=1;i<10; i+=2) {
+console.log(i)
+if(i==9){
+    break;
+}
+}
+for(let i=2;i<=10; i+=2) {
+    console.log(i)
+    
+    }
+
+//Ex 3
+for(let i=6;i<=60; i+=3) {
+    console.log(i)
+    
+
+}
+// ex 4
+let str=" "
+for (let i=1; i<8;i++){
+   
+   str += "#"
+   console.log(str)
+}
+//Ex 5 print even number
+for(let i=1;i<=10;i++){
+
+   let j=i % 2
+    if(j==0){
+        console.log(i)
+    }
+
+}
+
+//prime ,even and odd number
+// for(let i=1;i<=20;i++){
+//     if(i==1 ||i ==3){
+//         console.log(i+" odd")
+//     }else if(i==2){
+//         console.log(i=' even')
+//     }
+//     else
+//     if(i%2==0){
+//         console.log(i+ " even")
+
+//     }else if(i%2==1){
+//         console.log(i+" odd")
+//     }
+//     else{
+//         console.log(i+" prime")
+//     }
+
+//no}
+
+//while loop
+console.log("While loop")
+let i=3
+while(i<35){
+    console.log(i)
+    i=i+3
+}
+
+//multiple of 5
+let j = 5
+while(j<100){
+    console.log(j)
+    j=j+5
+}
+
+// ex 3
+
+k=0
+while(k<20){
+    if(k %2 ==0){
+        k=k*3
+        console.log(k)
+        
+    
+    
+    }
+    k++
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
