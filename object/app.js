41ad2b31dba843bb3b417c6783b9cccdbeb7c7c0
Priyanka Jain <priyankajain@Viveks-MacBook-Pro.local>
2022-08-16 22:38:51 -0400

    finished Largest denomination pgm

--- object/app.js
+++ object/app.js
@@ -44,8 +44,8 @@ const charecterCount = str =>{
     
     for (let i =0 ;i < str.length ; i ++){
         let char=str[i]
-        
-        //console.log(output)
+        console.log(char)
+        //console.log(output[char])
         if (output[char]){
             output[char]++
         }
@@ -59,4 +59,117 @@ const charecterCount = str =>{
 }
 charecterCount('hello')
 
-// 
+// largest denomination
+let denomination={}
+
+
+
+function lDenomination(cost1, paid1){
+     recAmount1 = paid1 - cost1
+    recAmount1  =   recAmount1  *   100
+    //console.log(recAmount1)
+    while (recAmount1   >   0){
+        if (recAmount1 >=   10000){
+            recAmount1 = recAmount1 -10000
+                if (denomination.hundred_count){
+                    denomination.hundred_count = denomination.hundred_count + 1
+                    }else{
+                        denomination.hundred_count = 1
+                }
+           }
+        else if (recAmount1 >=5000){
+             recAmount1 = recAmount1 -5000
+                 if (  denomination.fifty_count){
+                    denomination.fifty_count = denomination.fifty_count + 1
+                 }
+                else {
+                    denomination.fifty_count = 1
+                 }
+             
+            }
+        else if (recAmount1 >= 2000){
+            recAmount1 = recAmount1 - 2000
+                if (denomination.twenty_count){
+                        denomination.twenty_count =  denomination.twenty_count +1
+                }
+                else {
+                    denomination.twenty_count = 1
+                }
+
+            }
+        else if (recAmount1 >= 1000){
+            recAmount1 = recAmount1 - 1000
+            denomination.ten_count =  denomination.ten_count + 1
+            if (denomination.ten_count){
+                denomination.ten_count =  denomination.ten_count + 1
+            }else{
+                denomination.ten_count = 1
+            }
+
+        }
+        else if (recAmount1 >= 500) {
+            recAmount1 = recAmount1 - 500
+            if (  denomination.five_count){
+            denomination.five_count= denomination.fifty_count + 1
+            } else {
+                denomination.five_count = 1
+            }
+           
+        }
+        
+        else if (recAmount1 >=100) {
+            recAmount1 = recAmount1 - 100
+            if (  denomination.one_count){
+                denomination.one_count= denomination.one_count + 1
+                } else {
+                    denomination.one_count = 1
+                }
+           
+        }
+       
+
+        else if (recAmount1 >= 25) {
+            recAmount1 = recAmount1 - 25
+            if (  denomination.quater_count){
+                denomination.quater_count= denomination.quater_count + 1
+                } else {
+                    denomination.quater_count = 1
+                }
+        }
+        
+        else if (recAmount1 >= 10) {
+            recAmount1 = recAmount1 - 10
+            if (  denomination.dime_count){
+                denomination.dime_count= denomination.dime_count + 1
+                } else {
+                    denomination.dime_count = 1
+                }
+        }
+       
+        else if (recAmount1 >= 5 ){
+            recAmount1 = recAmount1 - 5
+            if (  denomination.nickle_count){
+                denomination.nickle_count= denomination.nickle_count + 1
+                } else {
+                    denomination.nickle_count = 1
+                }
+        }
+        
+
+        else if (recAmount1 >=1 ){
+            recAmount1 = recAmount1 - 1
+            if (  denomination.penny_count){
+                denomination.penny_count= denomination.penny_count + 1
+                } else {
+                    denomination.penny_count = 1
+                }
+        }
+        //return recAmount1
+    }
+    return console.log(denomination)
+}
+    lDenomination(25.47,140)
+    //console.log("Hundres : "+denomination.hundred_count)
+
+
+
