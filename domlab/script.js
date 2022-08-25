0b8cbf2302ad1c4fff5f67ffc8be1c5a439214ac
Priyanka Jain <priyankajain@Viveks-MacBook-Pro.local>
2022-08-24 21:58:46 -0400

    Where is Waldo commited

--- domlab/script.js
+++ domlab/script.js
@@ -4,6 +4,7 @@ var menuLinks = [
     {text: 'orders', href: '/orders'},
     {text: 'account', href: '/account'},
   ];
+  
   let mainEl =document.querySelector("main")
   
   mainEl.style.backgroundColor = "#4a4e4d"
@@ -11,7 +12,17 @@ var menuLinks = [
 
   mainEl.setAttribute("class","flex-ctr")
 
-  let topMenuEl =document.querySelector("nav")
+  let topMenuEl =document.querySelector("#top-menu")
   topMenuEl.style.height="100%"
-  topMenuEl.style.backgroundColor = ""
+  topMenuEl.style.backgroundColor = "var(--sub-menu-bg)"
+ topMenuEl.setAttribute("class","flex-around")
+
+ for (item of menuLinks){
+  let newtag = document.createElement("a")
+  newtag.setAttribute("href",item.href)
+  newtag.innerText = item.text
+  topMenuEl.appendChild(newtag)
+
+ }
+
 
