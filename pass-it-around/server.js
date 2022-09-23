92c6cf19a8a723925df34e6e44da1cf3b9238cef
Priyanka Jain <priyankajain@Viveks-MacBook-Pro.local>
2022-09-23 17:40:31 -0400

    express homework

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,35 +1,49 @@
-const express = require("express")
-const port = 3000
-const app = express()
 
-const random = Math.floor(Math.random(100-1))
+//gettng express package
+const express = require("express");
+//declaring the port
+const port = 3000;
 
-app.get('/',(req,res) =>{
-    res.send("<h1>99 Bottles of Soda on the wall</h1><h2><a href='/98'</a>take one out pass it around</h2>")
-})
+//declaring expree as a function
+const app = express();
 
 
-app.get('/:number_of_bottles',(req,res) =>{
-    for(i= req.params.number_of_bottles; i >=0 ; i--){ 
-   if (i ==0){
-    
-        res.send("<h1>99 Bottles of Soda on the wall</h1><h2><a href='/98'</a>take one out pass it around</h2")
-      return
-   
-   }else if(Math.random() > .85){
-  
-    res.send(`<h1>${req.params.number_of_bottles} Bottles of 
-    Soda on the wall</h1><h2><a href='/${parseInt(req.params.number_of_bottles) + 1}'
-    </a> Add one more</h2>`)}
-    else{
-        res.send(`<h1>${req.params.number_of_bottles} Bottles of 
-        Soda on the wall</h1><h2><a href='/${parseInt(req.params.number_of_bottles)-1}'
-        </a>take one out pass it around</h2>`)}
+//respondinding the request for 99 soda bottles
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>99 Bottles of Soda on the wall</h1><h2><a href='/98'</a>take one out pass it around</h2>"
+  );
+});
+
+//providing the link for decreasing or increasing number of soda bottles 
+//depending on random number generation. If random number is more than .85 then one soda
+//bottle would be added else if number is less than .85 then one 
+//soda bottle would be taken down an d pass it around. once counter reaches to
+//0 then cycle would restart
+
+app.get("/:number_of_bottles", (req, res) => {
+  for (i = req.params.number_of_bottles; i >= 0; i--) {
+    if (i == 0) {
+      res.send(
+        "<h1>99 Bottles of Soda on the wall</h1><h2><a href='/98'</a>take one out pass it around</h2"
+      );
+      return;
+    } else if (Math.random() > 0.85) {
+      res.send(`<h1>${req.params.number_of_bottles} Bottles of 
+    Soda on the wall</h1><h2><a href='/${
+      parseInt(req.params.number_of_bottles) + 1
+    }'
+    </a> Add one more and let the counter be more</h2>`);
+    } else {
+      res.send(`<h1>${req.params.number_of_bottles} Bottles of 
+        Soda on the wall</h1><h2><a href='/${
+          parseInt(req.params.number_of_bottles) - 1
+        }'
+        </a>take one out pass it around</h2>`);
     }
-  
-
-})
-
-app.listen(port, ()=>{
-    console.log("running server on port ...",port)
-})
+  }
+});
+ //listening to the port
+app.listen(port, () => {
+  console.log("running server on port ...", port);
+});
